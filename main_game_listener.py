347dc752188a4c46b3d7ef3a6d54f40d4d4d2ca0
Ole halvor dahl <Olli408@hotmail.com>
2020-06-13 18:10:14 +0200

    more

--- main_game_listener.py
+++ main_game_listener.py
@@ -11,7 +11,6 @@ b.connect()
 b.get_api()
 print(json.dumps(b.get_api()['lights'], indent=4, sort_keys=True))
 
-
 # If color is not same as last set: Save this color as the base color
 history_of_set_temperatures = []
 
@@ -19,9 +18,9 @@ from programs import programs
 
 print(programs)
 
-
 current_app = None
 running_apps = []
+
 
 def find_procs_by_name(name):
     "Return a list of processes matching 'name'."
@@ -33,19 +32,46 @@ def find_procs_by_name(name):
             ls.append(p)
     return ls
 
-def get_current_temperature():
-    print("returning 100 hardiced current temp")
-    return 100
+def flash():
+    flash_duration = 0.2
+    previous_brightnesses = get_current_brightness()
+    setBrightness(254)
+    time.sleep(flash_duration)
+    setBrightness(0)
+    time.sleep(flash_duration)
+    setBrightness(previous_brightnesses)
 
-max_temperature = 999
-base_temperature = 250
-last_set_temperature = base_temperature
-last_command = None
+def get_current_temperature():
+    for light in lightsToControl:
+        return b.get_light(light)['state']['ct']
+
+
+def get_current_brightness():
+    for light in lightsToControl:
+        return b.get_light(light)['state']['bri']
+
 
 def setColorTemperature(ct):
+    if ct < 0:
+        ct = 0
+    print("setting color",ct)
     for light in lightsToControl:
-        last_set_temperature = ct
         b.set_light(light, 'ct', ct)
+
+
+def setBrightness(bri):
+    print("setting brgihtness",bri)
+    for light in lightsToControl:
+        b.set_light(light, 'bri', bri)
+
+
+base_brightness = get_current_brightness()  # 1 - 254
+last_set_brightness = get_current_brightness()
+bri_last_iteration = get_current_brightness()
+
+base_temperature = get_current_temperature()  # 155 - 453
+last_set_temperature = get_current_temperature()
+temp_last_iteration = get_current_temperature()
 
 current_app = "ingen"
 while True:
@@ -59,27 +85,56 @@ while True:
 
     for running_app in running_apps:
         if running_app not in found_apps:
-            print("App has closed: ",running_app)
+            print("App has closed: ", running_app)
             running_apps.remove(running_app)
             break
     for app in found_apps:
         if app not in running_apps:
             new_apps.append(app)
             running_apps.append(app)
-            print("new app found: ",app)
-            break
+            print("new app found: ", app)
 
+    for app in new_apps:
+        if app["flash"]:
+            flash()
+            break
     if len(found_apps) > 0:
         highest_priority_app = found_apps[0]
         if not current_app == highest_priority_app:
-            setColorTemperature(highest_priority_app["color_temperature"])
+            # New highest priority app detected, change lights
+            if "color_temperature" in highest_priority_app:
+                last_set_temperature = highest_priority_app["color_temperature"]
+                setColorTemperature(highest_priority_app["color_temperature"])
+            if "brightness" in highest_priority_app:
+                last_set_brightness = highest_priority_app["brightness"]
+                setBrightness(highest_priority_app["brightness"])
             current_app = highest_priority_app
-            print("setting",highest_priority_app)
-        elif not get_current_temperature() == last_set_temperature:
-            print("external temperature detected, setting new base")
-            base_temperature = get_current_temperature()
+            print("new priority app", highest_priority_app)
+        else:
+            if current_app == highest_priority_app:
+                if "color_temperature" in highest_priority_app and not get_current_temperature() == highest_priority_app["color_temperature"]:
+                    print("external temperature detected, saving new base value")
+                    base_temperature = get_current_temperature()
+                    base_brightness = get_current_brightness()
+                if "brightness" in highest_priority_app and not get_current_brightness() == highest_priority_app["brightness"]:
+                    # Not a new app, but missmatch of brightness
+                    print("external brightness detected, saving new base value")
+                    base_brightness = get_current_brightness()
     elif not current_app == "base":
         print("setting base")
+        last_set_temperature = base_temperature
         setColorTemperature(base_temperature)
-        current_app="base"
+        last_set_brightness = base_brightness
+        setBrightness(base_brightness)
+        current_app = "base"
+
+    if len(found_apps) == 0 and last_set_brightness == base_brightness and not get_current_brightness() == last_set_brightness:
+        print("external brightness detected, saving new base value")
+        base_brightness = get_current_brightness()
+    if len(found_apps) == 0 and last_set_temperature == base_temperature and not get_current_temperature() == last_set_temperature:
+        print("external temperature detected, saving new base value")
+        base_temperature = get_current_temperature()
+
     time.sleep(1)
+    bri_last_iteration = get_current_brightness()
+    temp_last_iteration = get_current_temperature()
--- programs.py
+++ programs.py
@@ -3,21 +3,35 @@ programs = []
 p = {}
 p["name"] = "HWInfo"
 p["process_name"] = "HWiNFO64.EXE"
-p["color_temperature"] = 1
+p["color_temperature"] = 200
 p["priority"] = 10
+p["flash"] = False
 programs.append(p)
 
-p = {}
-p["name"] = "Steam"
-p["process_name"] = "steam.exe"
-p["color_temperature"] = 99
-p["priority"] = 15
-programs.append(p)
+s = {}
+s["name"] = "Steam"
+s["process_name"] = "steam.exe"
+#p["color_temperature"] = 454
+#p["brightness"] = 50
+s["priority"] = 15
+s["flash"] = False
+programs.append(s)
 
 doom_eternal = {}
 doom_eternal["name"] = "DOOM Eternal"
-doom_eternal["process_name"] = "doometernalx64vk.exe"
-doom_eternal["color_temperature"] = 999
+doom_eternal["process_name"] = "DOOMEternalx64vk.exe"
+doom_eternal["color_temperature"] = 454
+doom_eternal["brightness"] = 50
 doom_eternal["priority"] = 5
+doom_eternal["flash"] = False
+programs.append(doom_eternal)
+
+doom_eternal = {}
+doom_eternal["name"] = "Clone Hero"
+doom_eternal["process_name"] = "Clone Hero.exe"
+doom_eternal["color_temperature"] = 155
+#doom_eternal["brightness"] = 50
+doom_eternal["priority"] = 6
+doom_eternal["flash"] = False
 programs.append(doom_eternal)
 
